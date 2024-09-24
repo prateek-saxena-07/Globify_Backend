abf840fc6e1544364be8bdfae6f39dab1d1bcdfb
Prateek <86514419+prateek-saxena-07@users.noreply.github.com>
2024-09-25 01:49:17 +0530

    Basic_structure_commit

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import productRouter from './routes/product.routes.js';
 import cartRouter from './routes/cart.routes.js'
-
 dotenv.config();
 
+const PORT=process.env.PORT||5100
 const app = new express();
+
 app.use(express.json());
 app.use('/products',productRouter);
 app.use('/cart', cartRouter);
 
-const PORT=5100
+
 app.listen(PORT, () =>
 {   
     console.log(`Server Started at ${PORT}`);
