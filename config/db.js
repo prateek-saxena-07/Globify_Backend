e15983b1479d3ef518a3f9526fb6fc87e3637ca4
Prateek <86514419+prateek-saxena-07@users.noreply.github.com>
2024-09-27 03:04:14 +0530

    Empty commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-export const connectDB = async() => {
+export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log('Mongoose Connected :', conn.connection.host);
@@ -9,4 +9,4 @@ export const connectDB = async() => {
         console.error(`Error :${error.message}`);
         process.exit(1);
     }
-}
+};
